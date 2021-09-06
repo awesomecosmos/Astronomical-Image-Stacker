a90d6bf6728aec93d4eb5ecaba9515e8e9c43bb2
Aayushi Verma <aayushigeekgirl@gmail.com>
2021-09-06 21:39:57 +1200

    update

--- src/sidereal_image_stacker.py
+++ src/sidereal_image_stacker.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 # SIS = Sidereal Image Stacker
 # NIS = Non-sidereal Image Stacker
 
@@ -17,50 +18,98 @@ from drp_funcs import *
 from asp_funcs import *
 
 # initialising starting directory
-sis_code_path = "C:/Users/ave41/OneDrive - University of Canterbury/Master's 2021/ASTR480 Research/ASTR480 Code/03 Photometrical Analysis"
+sis_code_path = "C:/Users/ave41/OneDrive - University of Canterbury/Master's 2021/ASTR480 Research/ASTR480 Code/02 Data Analysis/Image-Stacker/src"
 os.chdir(sis_code_path) #from now on, we are in this directory
 
 # importing functions
 from sis_funcs import *
 
 #%%
-# defining paths/reading in data files
-test_data_path = "//spcsfs/ave41/astro/ave41/SIS_TestData_v1"
+# vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+# vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv CHANGES vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+# vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+# defining paths
+# test_data_path = "//spcsfs/ave41/astro/ave41/SIS_TestData_v1"
 data_path = "//spcsfs/ave41/astro/ave41/ObsData-2021-02-18/ALERT/Reduced ALERT/WCS Calibrated"
 
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ CHANGES ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#%%
+# defining paths
 SIS_path = Path(data_path)
 SIS_stack_path = path_checker(SIS_path,'Sidereally Stacked Images')
 outputs_path = path_checker(SIS_stack_path,'Outputs')
 
-#%%
+# reading in data files
 lst_of_files = [n for n in os.listdir(SIS_path) if (n.endswith('fit'))]
-
 data_lst = []
 for i in lst_of_files:
     data_lst.append(os.path.join(SIS_path,i))
 
 #%%
+# aligning images
 source_img = data_lst[0]
 source_img_hdu = fits.open(source_img)
 source_img_data = source_img_hdu[0].data
-    
+  
+registered_image_ccd_lst = []
+  
 for i in range(len(data_lst)):
     target_img = data_lst[i]
     target_img_hdu = fits.open(target_img)
     target_img_data = target_img_hdu[0].data
     
+    # checking for endian-ness of data
+    # my machine is a little-endian compiler so need to convert from big-endian
     if not source_img_data.dtype.byteorder == '<':
         source_img_data2 = source_img_data.byteswap().newbyteorder()
         target_img_data2 = target_img_data.byteswap().newbyteorder()
     
         registered_image, footprint = aa.register(source_img_data2, target_img_data2)
-        registered_img_writer(target_img,registered_image,SIS_stack_path)
+        registered_image_ccd = registered_img_writer(target_img,registered_image,SIS_stack_path)
+        registered_image_ccd_lst.append(registered_image_ccd)
 
     else:
         registered_image, footprint = aa.register(source_img_data, target_img_data)
-        registered_img_writer(target_img,registered_image,SIS_stack_path)
+        registered_image_ccd = registered_img_writer(target_img,registered_image,SIS_stack_path)
+        registered_image_ccd_lst.append(registered_image_ccd)
 
+#%%
+registered_image_ccd_data = []
+for i in registered_image_ccd_lst:
+    registered_image_ccd_data.append(i.data)
 
+# plotting stats for each aligned image
+aligned_img_stats(registered_image_ccd_data,outputs_path)
+#%%
+source_img_hdr1 = source_img_hdu[0].header
+
+target_name = source_img_hdr1['FIELD'].strip(' ')
+exptime = source_img_hdr1['EXPTIME']
+filter_colour = source_img_hdr1['COLOUR'].strip(' ')
+obs_set = source_img_hdr1['SET'].strip(' ')
+chip_num = source_img_hdr1['CHIP']
+
+filename_to_write = "stacked-{}-{}-{}-{}-{}.fit".format(target_name,exptime,
+                                                        filter_colour,obs_set,
+                                                        chip_num)
+
+stacked_img = sum(registered_image_ccd_data) / len(registered_image_ccd_data)
+stacked_img_ccd = CCDData(stacked_img,unit='adu')
+stacked_img_ccd.header = source_img_hdr1
+       
+stacked_img_ccd.write(SIS_stack_path/filename_to_write,overwrite=True)
+
+#%%
+plt.figure()
+sns.set_theme(style="whitegrid")
+ax = sns.violinplot(x=stacked_img.flatten(),color="darkviolet")
+ax.set_title('Distribution of counts for stacked image')
+ax.set_xlabel('Counts')
+plt.savefig(outputs_path/"stacked_stats_violin.jpg",dpi=900)
+plt.show()
 
 
 
--- src/sis_funcs.py
+++ src/sis_funcs.py
@@ -10,15 +10,37 @@
 #-------------------SECTION ONE: IMPORTING PACKAGES---------------------------#
 ###############################################################################
 
-import astroalign as aa
-import astropy.units as u
-from astropy.io import fits
-from astropy.nddata import CCDData
-
 # path-type packages
 import os
 import glob
 from pathlib import Path
+
+# basic Python packages
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+# Astropy packages
+import astropy
+import astroalign as aa
+from astropy.io import fits
+from astropy.wcs import WCS
+import astropy.units as u
+from astropy.stats import mad_std
+from astropy.nddata import CCDData
+from astropy.visualization import hist
+from astropy.utils.data import get_pkg_data_filename
+
+# ccdproc packages
+import ccdproc as ccdp
+from ccdproc import Combiner
+from ccdproc import ImageFileCollection
+from ccdproc.utils.sample_directory import sample_directory_with_files
+
+# Misc. packages
+import seaborn as sns
+from datetime import datetime
 
 ###############################################################################
 #--------------------SECTION ONE: HELPER FUNCTIONS----------------------------#
@@ -42,7 +64,8 @@ def registered_img_writer(img,registered_img,SIS_stack_path):
     
     Returns
     -------
-    Nothing.
+    registered_img_ccd : astropy.nddata.ccddata.CCDData
+        CCDData object of registered image.
     """
     img_hdul = fits.open(img)
     img_hdr1 = img_hdul[0].header
@@ -54,10 +77,52 @@ def registered_img_writer(img,registered_img,SIS_stack_path):
     obs_set = img_hdr1['SET'].strip(' ')
     chip_num = img_hdr1['CHIP']
                 
-    filename_to_write = "stacked-{}-{}-{}-{}-{}-{}.fit".format(run_filename,target_name,exptime,
-                                                                   filter_colour,obs_set,
-                                                                        chip_num)
+    filename_to_write = "aligned-{}-{}-{}-{}-{}-{}.fit".format(run_filename,target_name,
+                                                               exptime,filter_colour,
+                                                               obs_set,chip_num)
         
     registered_img_ccd = CCDData(registered_img,unit='adu')
     registered_img_ccd.header = img_hdr1
     registered_img_ccd.write(SIS_stack_path/filename_to_write,overwrite=True)
+    
+    return registered_img_ccd
+
+def aligned_img_stats(aligned_image_ccd_lst,plots_path):
+    median_count = []
+    mean_count = []
+    for a_file in aligned_image_ccd_lst:
+        hdu = CCDData(a_file,unit='adu')
+        image_data = hdu.data.astype(float) 
+        image_hdr = hdu.header
+        
+        median_count.append(np.median(a_file))
+        mean_count.append(np.mean(a_file))
+    
+    min_count_for_median = np.min(median_count)
+    min_count_for_mean = np.min(mean_count)
+    max_count_for_median = np.max(median_count)
+    max_count_for_mean = np.max(mean_count)
+    
+    plt.figure()
+    plt.plot(mean_count, label='mean',color="palevioletred")
+    plt.axhline(y=min_count_for_mean,linestyle='-',linewidth=0.5,color='blue',label='min mean {:.2f}'.format(min_count_for_mean),alpha=1)
+    plt.axhline(y=max_count_for_mean,linestyle='-',linewidth=0.5,color='blue',label='max mean {:.2f}'.format(max_count_for_mean),alpha=1)
+    plt.xlabel('Image number')
+    plt.ylabel('Count (ADU)')
+    plt.title('Mean pixel value for aligned images')
+    plt.legend()
+    plt.grid()
+    plt.savefig(plots_path/"aligned_stats_mean.jpg",dpi=900)
+    plt.show()
+
+    plt.figure()
+    plt.plot(median_count, label='median',color="darkviolet")
+    plt.axhline(y=min_count_for_median,linestyle='-',linewidth=0.5,color='red',label='min median {:.2f}'.format(min_count_for_median),alpha=1)
+    plt.axhline(y=max_count_for_median,linestyle='-',linewidth=0.5,color='red',label='max median {:.2f}'.format(max_count_for_median),alpha=1)               
+    plt.xlabel('Image number')
+    plt.ylabel('Count (ADU)')
+    plt.title('Median pixel value for aligned images')
+    plt.legend()
+    plt.grid()
+    plt.savefig(plots_path/"aligned_stats_median.jpg",dpi=900)
+    plt.show()

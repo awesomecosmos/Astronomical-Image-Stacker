b80aaba2302d068edc18a58a3c949887c7557871
awesomecosmos <56360465+awesomecosmos@users.noreply.github.com>
2021-09-07 13:29:23 +1200

    update

--- src/sidereal_image_stacker.py
+++ src/sidereal_image_stacker.py
@@ -7,6 +7,11 @@
 #-------------------SECTION ZERO: IMPORTING PACKAGES--------------------------#
 ###############################################################################
 
+# initialising timer so we can count elapsed time
+from pytictoc import TicToc
+t = TicToc() # create TicToc instance
+t.tic() # Start timer
+
 import os
 
 # initialising starting directory
@@ -25,13 +30,21 @@ os.chdir(sis_code_path) #from now on, we are in this directory
 from sis_funcs import *
 
 #%%
+###############################################################################
+#----------------------SECTION ONE: INITIALISATION----------------------------#
+###############################################################################
+
+
 # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv CHANGES vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
+# defining observation date for display/saving purposes
+obsdate = "2021-02-13"
+
 # defining paths
 # test_data_path = "//spcsfs/ave41/astro/ave41/SIS_TestData_v1"
-data_path = "//spcsfs/ave41/astro/ave41/ObsData-2021-02-18/ALERT/Reduced ALERT/WCS Calibrated"
+data_path = "//spcsfs/ave41/astro/ave41/ObsData-2021-02-13/ALERT/Reduced ALERT/WCS Calibrated"
 
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ CHANGES ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -48,8 +61,21 @@ data_lst = []
 for i in lst_of_files:
     data_lst.append(os.path.join(SIS_path,i))
 
+# making list of CCDData objects of these non-aligned images
+data_lst_images = []
+for i in data_lst:
+    img = fits.open(i)
+    img_hdr = img[0].header
+    data_array = CCDData(img[0].data,unit='adu')
+    data_array.header = img_hdr
+    data_lst_images.append(data_array)
+
 #%%
-# aligning images
+###############################################################################
+#---------------------SECTION TWO: ALIGNING IMAGES----------------------------#
+###############################################################################
+
+# aligning/registering images
 source_img = data_lst[0]
 source_img_hdu = fits.open(source_img)
 source_img_data = source_img_hdu[0].data
@@ -68,72 +94,64 @@ for i in range(len(data_lst)):
         target_img_data2 = target_img_data.byteswap().newbyteorder()
     
         registered_image, footprint = aa.register(source_img_data2, target_img_data2)
-        registered_image_ccd = registered_img_writer(target_img,registered_image,SIS_stack_path)
+        registered_image_ccd = registered_img_writer(target_img,registered_image,SIS_stack_path,obsdate)
         registered_image_ccd_lst.append(registered_image_ccd)
 
     else:
         registered_image, footprint = aa.register(source_img_data, target_img_data)
-        registered_image_ccd = registered_img_writer(target_img,registered_image,SIS_stack_path)
+        registered_image_ccd = registered_img_writer(target_img,registered_image,SIS_stack_path,obsdate)
         registered_image_ccd_lst.append(registered_image_ccd)
 
-#%%
 registered_image_ccd_data = []
 for i in registered_image_ccd_lst:
     registered_image_ccd_data.append(i.data)
 
-# plotting stats for each aligned image
-aligned_img_stats(registered_image_ccd_data,outputs_path)
-#%%
-source_img_hdr1 = source_img_hdu[0].header
+# plotting stats for comapring non-aligned vs aligned images
+aligned_comparison_stats(data_lst_images,registered_image_ccd_lst,outputs_path,obsdate)
 
+#%%
+###############################################################################
+#---------------------SECTION THREE: STACKING IMAGES--------------------------#
+###############################################################################
+
+# defining source image and extracting information from source image
+source_img_hdr1 = source_img_hdu[0].header
 target_name = source_img_hdr1['FIELD'].strip(' ')
 exptime = source_img_hdr1['EXPTIME']
 filter_colour = source_img_hdr1['COLOUR'].strip(' ')
 obs_set = source_img_hdr1['SET'].strip(' ')
 chip_num = source_img_hdr1['CHIP']
 
-filename_to_write = "stacked-{}-{}-{}-{}-{}.fit".format(target_name,exptime,
+filename_to_write = "{}-stacked-{}-{}-{}-{}-{}.fit".format(obsdate,target_name,exptime,
                                                         filter_colour,obs_set,
                                                         chip_num)
-
+plot_filename_to_write = "{}-stacked-stats-violin-{}-{}-{}-{}-{}.jpg".format(obsdate,target_name,exptime,
+                                                        filter_colour,obs_set,
+                                                        chip_num)
+# stacking aligned images together
 stacked_img = sum(registered_image_ccd_data) / len(registered_image_ccd_data)
 stacked_img_ccd = CCDData(stacked_img,unit='adu')
 stacked_img_ccd.header = source_img_hdr1
-       
 stacked_img_ccd.write(SIS_stack_path/filename_to_write,overwrite=True)
 
-#%%
+# violin plot of stacked image counts
 plt.figure()
 sns.set_theme(style="whitegrid")
 ax = sns.violinplot(x=stacked_img.flatten(),color="darkviolet")
 ax.set_title('Distribution of counts for stacked image')
 ax.set_xlabel('Counts')
-plt.savefig(outputs_path/"stacked_stats_violin.jpg",dpi=900)
+plt.savefig(outputs_path/plot_filename_to_write,dpi=900)
 plt.show()
 
+#%%
+#================================ don't touch ================================#
 
+###############################################################################
+#-------------------------------END OF CODE-----------------------------------#
+###############################################################################
 
+t.toc() # Print elapsed time
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+###############################################################################
+#-------------------------------END OF CODE-----------------------------------#
+###############################################################################
--- src/sis_funcs.py
+++ src/sis_funcs.py
@@ -42,11 +42,15 @@ from ccdproc.utils.sample_directory import sample_directory_with_files
 import seaborn as sns
 from datetime import datetime
 
+# warnings
+import warnings
+warnings.filterwarnings('ignore')
+
 ###############################################################################
 #--------------------SECTION ONE: HELPER FUNCTIONS----------------------------#
 ###############################################################################
 
-def registered_img_writer(img,registered_img,SIS_stack_path):
+def registered_img_writer(img,registered_img,SIS_stack_path,obsdate):
     """
     This function writes the original FITS header to a FITS file of the 
     registered image.
@@ -61,6 +65,10 @@ def registered_img_writer(img,registered_img,SIS_stack_path):
     
     SIS_stack_path : WindowsPath object
         Path to directory where sidereally-stacked image is to be saved.
+    
+    obsdate : str
+        Date of observation for display/saving purposes. 
+        Ideal format should be "YYYY-MM-DD".
     
     Returns
     -------
@@ -77,7 +85,7 @@ def registered_img_writer(img,registered_img,SIS_stack_path):
     obs_set = img_hdr1['SET'].strip(' ')
     chip_num = img_hdr1['CHIP']
                 
-    filename_to_write = "aligned-{}-{}-{}-{}-{}-{}.fit".format(run_filename,target_name,
+    filename_to_write = "{}-aligned-{}-{}-{}-{}-{}-{}.fit".format(obsdate,run_filename,target_name,
                                                                exptime,filter_colour,
                                                                obs_set,chip_num)
         
@@ -87,10 +95,41 @@ def registered_img_writer(img,registered_img,SIS_stack_path):
     
     return registered_img_ccd
 
-def aligned_img_stats(aligned_image_ccd_lst,plots_path):
+###############################################################################
+#--------------------SECTION TWO: PLOTTING FUNCTIONS--------------------------#
+###############################################################################
+
+def img_series_stats(image_ccd_lst,plots_path,obsdate):
+    """
+    This function plots count statistics for a series of images.
+    
+    Parameters
+    ----------
+    image_ccd_lst : list
+        List of CCDData objects.
+    
+    plots_path : WindowsPath object
+        Path to directory where plots are to be saved.
+    
+    obsdate : str
+        Date of observation for display/saving purposes. 
+        Ideal format should be "YYYY-MM-DD".
+    
+    Returns
+    -------
+    Nothing.
+    """
     median_count = []
     mean_count = []
-    for a_file in aligned_image_ccd_lst:
+    
+    source_hdu = CCDData(image_ccd_lst[0],unit='adu')
+    source_image_data = source_hdu.data.astype(float) 
+    source_image_hdr = source_hdu.header
+    target_name = source_image_hdr['FIELD'].strip(' ')
+    exptime = source_image_hdr['EXPTIME']
+    chip_num = source_image_hdr['CHIP']
+        
+    for a_file in image_ccd_lst:
         hdu = CCDData(a_file,unit='adu')
         image_data = hdu.data.astype(float) 
         image_hdr = hdu.header
@@ -112,7 +151,10 @@ def aligned_img_stats(aligned_image_ccd_lst,plots_path):
     plt.title('Mean pixel value for aligned images')
     plt.legend()
     plt.grid()
-    plt.savefig(plots_path/"aligned_stats_mean.jpg",dpi=900)
+    plt.savefig(plots_path/"{}-{}-{}-aligned_stats_mean.jpg".format(obsdate,
+                                                                    target_name,
+                                                                    exptime,chip_num),
+                                                                    dpi=900)
     plt.show()
 
     plt.figure()
@@ -124,5 +166,137 @@ def aligned_img_stats(aligned_image_ccd_lst,plots_path):
     plt.title('Median pixel value for aligned images')
     plt.legend()
     plt.grid()
-    plt.savefig(plots_path/"aligned_stats_median.jpg",dpi=900)
+    plt.savefig(plots_path/"{}-{}-{}-aligned_stats_median.jpg".format(obsdate,
+                                                                      target_name,
+                                                                      exptime,chip_num),
+                                                                      dpi=900)
     plt.show()
+
+def aligned_comparison_stats(unaligned_image_ccd_lst,aligned_image_ccd_lst,
+                             plots_path,obsdate):
+    """
+    This function plots count statistics for a series of images to compare
+    non-aligned and aligned images.
+    
+    Parameters
+    ----------
+    unaligned_image_ccd_lst : list
+        List of CCDData objects of non-aligned images.
+    
+    aligned_image_ccd_lst : list
+        List of CCDData objects of aligned images.
+    
+    plots_path : WindowsPath object
+        Path to directory where plots are to be saved.
+    
+    obsdate : str
+        Date of observation for display/saving purposes. 
+        Ideal format should be "YYYY-MM-DD".
+    
+    Returns
+    -------
+    Nothing.
+    """
+    aligned_mean_count = []
+    aligned_median_count = []
+    unaligned_mean_count = []
+    unaligned_median_count = []
+    
+    # getting information from source image for saving purposes
+    source_hdu = CCDData(aligned_image_ccd_lst[0],unit='adu')
+    source_image_data = source_hdu.data.astype(float) 
+    source_image_hdr = source_hdu.header
+    target_name = source_image_hdr['FIELD'].strip(' ')
+    exptime = source_image_hdr['EXPTIME']
+    chip_num = source_image_hdr['CHIP']
+    
+    for a_file in unaligned_image_ccd_lst:
+        hdu = CCDData(a_file,unit='adu')
+        image_data = hdu.data.astype(float) 
+        image_hdr = hdu.header
+        
+        unaligned_mean_count.append(np.mean(a_file))
+        unaligned_median_count.append(np.median(a_file))
+    
+    min_count_for_unaligned_mean = np.min(unaligned_mean_count)
+    max_count_for_unaligned_mean = np.max(unaligned_mean_count)
+    min_count_for_unaligned_median = np.min(unaligned_median_count)
+    max_count_for_unaligned_median = np.max(unaligned_median_count)
+        
+    for a_file in aligned_image_ccd_lst:
+        hdu = CCDData(a_file,unit='adu')
+        image_data = hdu.data.astype(float) 
+        image_hdr = hdu.header
+        
+        aligned_mean_count.append(np.mean(a_file))
+        aligned_median_count.append(np.median(a_file))
+    
+    min_count_for_aligned_mean = np.min(aligned_mean_count)
+    max_count_for_aligned_mean = np.max(aligned_mean_count)
+    min_count_for_aligned_median = np.min(aligned_median_count)
+    max_count_for_aligned_median = np.max(aligned_median_count)
+    
+    # plotting
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(15,8),sharex=True, sharey=False)
+    fig.suptitle('Comparison Statistics for Non-Aligned Images vs Aligned Images for {}'.format(obsdate))
+    
+    # unaligned mean
+    ax1.plot(unaligned_mean_count, label='mean',color="deeppink")
+    ax1.axhline(y=min_count_for_unaligned_mean,linestyle='-',linewidth=1,
+                color='black',label='min mean {:.2f}'.format(min_count_for_unaligned_mean),alpha=1)
+    ax1.axhline(y=max_count_for_unaligned_mean,linestyle='-',linewidth=1,
+                color='black',label='max mean {:.2f}'.format(max_count_for_unaligned_mean),alpha=1)
+    ax1.set_xlabel('Image number')
+    ax1.set_ylabel('Count (ADU)')
+    ax1.set_title('Mean pixel value for unaligned images')
+    ax1.legend(loc="best")
+    ax1.grid()
+    
+    # unaligned median
+    ax2.plot(unaligned_median_count, label='median',color="deeppink")
+    ax2.axhline(y=min_count_for_unaligned_median,linestyle='-',linewidth=1,
+                color='black',label='min median {:.2f}'.format(min_count_for_unaligned_median),alpha=1)
+    ax2.axhline(y=max_count_for_unaligned_median,linestyle='-',linewidth=1,
+                color='black',label='max median {:.2f}'.format(max_count_for_unaligned_median),alpha=1)
+    ax2.set_xlabel('Image number')
+    ax2.set_ylabel('Count (ADU)')
+    ax2.set_title('Median pixel value for unaligned images')
+    ax2.legend(loc="best")
+    ax2.grid()
+    
+    # aligned mean
+    ax3.plot(aligned_mean_count, label='mean',color="darkviolet")
+    ax3.axhline(y=min_count_for_aligned_mean,linestyle='-',linewidth=1,
+                color='black',label='min mean {:.2f}'.format(min_count_for_aligned_mean),alpha=1)
+    ax3.axhline(y=max_count_for_aligned_mean,linestyle='-',linewidth=1,
+                color='black',label='max mean {:.2f}'.format(max_count_for_aligned_mean),alpha=1)
+    ax3.set_xlabel('Image number')
+    ax3.set_ylabel('Count (ADU)')
+    ax3.set_title('Mean pixel value for aligned images')
+    ax3.legend(loc="best")
+    ax3.grid()
+    
+    # aligned median
+    ax4.plot(aligned_median_count, label='median',color="darkviolet")
+    ax4.axhline(y=min_count_for_aligned_median,linestyle='-',linewidth=1,
+                color='black',label='min median {:.2f}'.format(min_count_for_aligned_median),alpha=1)
+    ax4.axhline(y=max_count_for_aligned_median,linestyle='-',linewidth=1,
+                color='black',label='max median {:.2f}'.format(max_count_for_aligned_median),alpha=1)
+    ax4.set_xlabel('Image number')
+    ax4.set_ylabel('Count (ADU)')
+    ax4.set_title('Median pixel value for unaligned images')
+    ax4.legend(loc="best")
+    ax4.grid()
+    
+    for ax in fig.get_axes():
+        ax.label_outer()
+    
+    fig.savefig(plots_path/"{}-{}-{}-comparison_stats.jpg".format(obsdate,
+                                                                  target_name,
+                                                                  exptime,chip_num),
+                                                                    dpi=1000)
+    fig.show()
+    
+###############################################################################
+#-------------------------------END OF CODE-----------------------------------#
+###############################################################################
